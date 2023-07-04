d67613590fa799cd14257ad0b183252f56914d0b
Zaid Daghestani <zaid.daghestani@gmail.com>
2023-07-04 17:10:20 -0500

    leave gql in main repo

--- import-gql.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-rm -rf ./gql 2> /dev/null
-mkdir ./gql
-cp ../inappchat-js/libs/chat-common/src/lib/schema.graphql ./gql/schema.graphqls
-cp ../inappchat-js/libs/chat-sdk/src/lib/api/*.graphql ./gql/
